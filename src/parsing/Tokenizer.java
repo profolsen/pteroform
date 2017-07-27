9214c2feec4876e4cbb0f4e62a1eb7d2b81354f1
profolsen <olsenp@strose.edu>
2017-07-27 17:35:09 -0400

    documented Tokenizer.

--- src/parsing/Tokenizer.java
+++ src/parsing/Tokenizer.java
@@ -29,19 +29,37 @@ import grammar.Token;
 import java.util.HashSet;
 
 /**
- * Created by po917265 on 6/30/17.
+ * The Tokenizer class converts a string into tokens one at a time for the parser.
+ * This class is probably redundant.
+ * Java has a Tokenizer that is probably far more sophisticated.
  */
 public class Tokenizer {
 
     private String source;
     private HashSet<Terminal> ignore;
 
+    /**
+     * Creates a new tokenizer which reads the source string.
+     * Any token created by the tokens in the ignore set are ignored.
+     * These tokens can only be used to separate tokens returned by this Tokenizer.
+     * @param source the source string.
+     * @param ignore a set of token types to ignore.
+     */
     public Tokenizer(String source, HashSet<Terminal> ignore) {
         this.source = source;
         this.ignore = ignore;
         ignore();
     }
 
+    /**
+     * Returns the next token in the string.
+     * If there is more than one possible next token, the longest one is returned.
+     * This method may return null if no terminal from possibleParses matches the source string.
+     * @param possibleParses a set of possible token types which could come next.
+     * @param use indicates whether the read token should be removed from the beginning of the source string.
+     *           If use is true, the token will be removed.
+     * @return the next Token in the string.
+     */
     public Token next(HashSet<Terminal> possibleParses, boolean use) {
         Token best = null;
         for(Terminal t : possibleParses) {
@@ -57,13 +75,21 @@ public class Tokenizer {
         return best;
     }
 
+    /**
+     * Returns the token in the source string.
+     * This method will return null if t does not match the source string.
+     * @param t a token type to look for.
+     * @param use if use is true, the tokenizer will advance over the token in the source string.
+     * @return a token or null if t does not match the string.
+     */
     public Token next(Terminal t, boolean use) {
         HashSet<Terminal> arg = new HashSet<Terminal>();
         arg.add(t);
         return next(arg, use);
     }
 
-    public void ignore() {
+
+    private void ignore() {
         Token best = null;
         for(Terminal t : ignore) {
             Token hypothetical = t.parse(source);
@@ -76,6 +102,10 @@ public class Tokenizer {
         }
     }
 
+    /**
+     * Returns the current source string.
+     * @return the current source string.
+     */
     public String source() {
         return source;
     }
